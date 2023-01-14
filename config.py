5f5ea5dde9c42a783a296ec33ae1a1c21ce7b80e
Masood <m.sayyah@aasood.com>
2023-01-14 14:14:40 +0000

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
     RABBIT_HOST: str = os.getenv("RABBIT_HOST")
     RABBIT_PORT: str = int(os.getenv("RABBIT_PORT"))
     RABBIT_USER: str = os.getenv("RABBIT_USER")
-    RABBIT_PASSWORD: str = os.getenv("RABBIT_PASSWORD")
+    RABBIT_PASS: str = os.getenv("RABBIT_PASS")
 
 
 config = Config()
