e3f41e7f0dfd59ed50e98ce774fa8bd248d08d37
Masood <m.sayyah@aasood.com>
2023-01-14 13:46:35 +0000

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,6 @@ class Config:
     MONGO_HOST: str = os.getenv("MONGO_HOST")
     MONGO_PORT: int = int(os.getenv("MONGO_PORT"))
     MONGO_DB: str = os.getenv("MONGO_DB")
-    BASKET_COLLECTION: str = os.getenv("BASKET_COLLECTION")
 
     RABBIT_HOST: str = os.getenv("RABBIT_HOST")
     RABBIT_PORT: str = int(os.getenv("RABBIT_PORT"))
